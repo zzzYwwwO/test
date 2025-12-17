72e30e035e9a547d20f144318b2f9026dbc66b9b
liu <3132519428@qq.com>
2025-12-17 16:16:10 +0800

    update 密码md5加密

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 const base64 = require('base-64');
-// require('dotenv').config();
+require('dotenv').config();
+const crypto = require('crypto');
 const dir = require('path').resolve(__dirname, '../../.password');
 
 function _decodeCredentials(header) {
@@ -23,32 +24,28 @@ function auth(req, res, next) {
       userInfo.set(username, info);
     });
   // console.log('User-Pass', userInfo);
-  if (userInfo.has(username) && userInfo.get(username).password == password) {
+  if (
+    userInfo.has(username) &&
+    userInfo.get(username).password ==
+      hashPasswordWithSHA256(username, password)
+  ) {
     req.role = userInfo.get(username).role;
+    // 对密码做SHA256哈希
+    // const sha256Pwd = hashPasswordWithSHA256(username, password);
+    // console.log('123456 SHA256哈希后：', sha256Pwd);
     return next();
   }
 
   res.set('WWW-Authenticate', 'Basic realm="user_pages"');
   res.status(401).send('Authentication required!');
-
-  // if (
-  //   username === process.env.MONGOGUI_ADMIN_USERNAME &&
-  //   password === process.env.MONGOGUI_ADMIN_PASSWORD
-  // ) {
-  //   req.isAdmin = true;
-  //   return next();
-  // } else {
-  //   if (
-  //     username === process.env.MONGOGUI_USER_USERNAME &&
-  //     password === process.env.MONGOGUI_USER_PASSWORD
-  //   ) {
-  //     req.isAdmin = false;
-  //     return next();
-  //   }
-  //   res.set('WWW-Authenticate', 'Basic realm="user_pages"');
-  //   res.status(401).send('Authentication required!');
-  // }
 }
+
+const hashPasswordWithSHA256 = (username, password) => {
+  return crypto
+    .createHash('sha256')
+    .update(`${username}:${password}:${process.env.MD5_SECRET}`)
+    .digest('hex'); // 生成64位16进制摘要
+};
 
 module.exports = {
   auth,
